dce73bf967c7acf2b9a24363abca964b1a88f2b3
Sujal Shah <sujalshah28092004@gmail.com>
2023-07-20 21:12:06 +0530

    bug fix

--- src/utility/projectConstant.js
+++ src/utility/projectConstant.js
@@ -4,7 +4,7 @@
       title: "E-commerce Website",
       description: "Ecommerce Website: A feature-rich online platform built with MERN stack. It offers seamless shopping experiences.",
       tags: ["Nextjs", "Redux", "Express.js", "MongoDB", "RTK Query","Authentication", "API"],
-      imageUrl: "/../public/e-bucket.png",
+      imageUrl: "https://cdn.pixabay.com/photo/2022/03/25/02/11/online-shop-7090139_1280.png",
       link:'https://ecommerce-web-app-j9h3.vercel.app/'
     },
     {
@@ -13,7 +13,7 @@
       description:
         "A powerful tool built with React and TypeScript that generates secure and customizable passwords. Users can specify the length and character types, ...",
       tags: ["React", "Next.Js", "TypeScript","Random"],
-      imageUrl: "/../public/password-generator.png",
+      imageUrl: "https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHBhc3N3b3JkfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
       link:'https://64b7f0bf2c77331a5ea20417--calm-buttercream-b21751.netlify.app/'
     },
     {
@@ -22,16 +22,16 @@
       description:
         "The Todo project is a TypeScript-powered application with active, completed, and all filters. ",
       tags: ["Local Storage", "Next.Js", "TypeScript"],
-      imageUrl: "/../public/todo.png",
+      imageUrl: "https://images.unsplash.com/photo-1627850604058-52e40de1b847?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2073&q=80",
       link:'https://64b05c9a925899000873540b--candid-blini-9e11c7.netlify.app/'
     },
     {
       id: 4,
-      title: "Temperature App",
+      title: "Weather App",
       description:
-        "The Temperature website, built with TypeScript, provides real-time weather information",
+        "The Weather website, built with TypeScript, provides real-time weather information",
       tags: ["TypeScript", "Next.Js", "Api's Handling"],
-      imageUrl: "/../public/temperature.png",
+      imageUrl: "https://images.unsplash.com/photo-1530563885674-66db50a1af19?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8V2VhdGhlciUyMGFwcHxlbnwwfDB8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
       link:'https://weather-app-sigma-ochre.vercel.app/'
     },
    
